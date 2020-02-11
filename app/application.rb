d73c5ef70e6bb7076c02a8dda18807c65c0c253f
Matthew Jones <matts3371@gmail.com>
2020-02-11 00:21:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,8 @@ class Application
   
   def call(env)
     resp = Rack::Response.new 
-    t
-    if Time.hour < 12 
+    t = Time.now 
+    if t.hour < 12 
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
